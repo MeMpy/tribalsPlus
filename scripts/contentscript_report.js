1ee1ba54fee1e8f739e77f41ae380eeb18b8b153
MeMpy <rosariorenga@gmail.com>
2016-10-23 14:11:21 +0200

    general improvement: improved report object, improved how to store, improved all links

--- scripts/contentscript_report.js
+++ scripts/contentscript_report.js
@@ -1,8 +1,12 @@
 $(document).ready( function() {
 
-  var request = { table: $('#content_value .report_ReportAttack').parents('table.vis:first').html()};
-  chrome.runtime.sendMessage(request, function(response) {
-    console.log('response received');
-  });
+	var requestUrl = window.location.href;
+	var request = { 
+		url: requestUrl,
+		table: $('#content_value .report_ReportAttack').parents('table.vis:first').html()
+	};
+	chrome.runtime.sendMessage(request, function(response) {
+	console.log('response received');
+	});
 
 })
--- scripts/eventPage.js
+++ scripts/eventPage.js
@@ -1,27 +1,27 @@
 chrome.runtime.onMessage.addListener(
   function(request, sender, sendResponse) {
 
+  	var reportBaseUrl = extractBaseUrl(request.url);
     var report = $.parseHTML(request.table)[1];
 
     var reportObj = {};
-    //TODO add report id -> Can be null!!! 
-    //TODO add report link!   
-    reportObj.villageLink = $(report).find('#attack_info_def .village_anchor a:first').attr("href");
+
+    reportObj.id = getId(request.url);
+    reportObj.playerId = $(report).find('#attack_info_def .village_anchor').attr('data-player');
     reportObj.villageId = $(report).find('#attack_info_def .village_anchor').attr('data-id');
+    reportObj.links = getLinks(reportBaseUrl, reportObj.playerId, reportObj.villageId);
+
     reportObj.combatTime = getDate($(report).find('tr:eq(1)').find('td:last').text().trim());
-    reportObj.playerName = $( report ).find('#attack_info_def tr:first').find('a').text().trim();
-    reportObj.playerLink = $( report ).find('#attack_info_def tr:first').find('a').attr('href');
+    reportObj.playerName = $( report ).find('#attack_info_def tr:first').find('a').text().trim();    
     reportObj.destination = $( report ).find('#attack_info_def .village_anchor a:first').text();
     reportObj.coordinates = getCoordinates( reportObj.destination);
     reportObj.reportIcon = getIconFileName($(report).find('tr:first img').attr('src'));
     
-    var spiedResources = getResources($(report).find('#attack_spy_resources'));
-    var hauledResources = getResources($(report).find('#attack_results'));
+    var spiedResources = $(report).find('#attack_spy_resources');
+    var hauledResources = $(report).find('#attack_results');
     
-    reportObj.resources = {
-	    	 spied: spiedResources, 
-	    	 haul: hauledResources
-    	};
+    reportObj.resources = getResources(spiedResources, hauledResources);
+    reportObj.actions = getActions(reportBaseUrl, reportObj.villageId);
 
     // by passing an object you can define default values e.g.: []
 	chrome.storage.local.get({reports: {}}, function (result) {
@@ -29,17 +29,29 @@ chrome.runtime.onMessage.addListener(
 		// the input argument is ALWAYS an object containing the queried keys
     	// so we select the key we need
     	var reportsToStore = result.reports;
-    	if(reportsToStore[reportObj.villageId])
-    		reportsToStore[reportObj.villageId].push(reportObj)
-    	else
+    	if(reportsToStore[reportObj.villageId]){
+    		if(!reportsToStore[reportObj.villageId].find(reportsEquality, reportObj) ){    		
+	    		reportsToStore[reportObj.villageId].push(reportObj);
+	    		storeReports(reportsToStore);
+    		}
+    	}
+    	else{
     		reportsToStore[reportObj.villageId] = [reportObj];
-
-	    chrome.storage.local.set({reports: reportsToStore}, function() {
-	      // Notify that we saved.
-	      console.log('reports saved');
-	    });
+    		storeReports(reportsToStore);
+    	}
 	});
 });
+
+function getId(reportUrl){
+	return reportUrl.substring(reportUrl.lastIndexOf('=')+1);
+}
+
+function extractBaseUrl(reportUrl){
+	var parser = document.createElement('a');
+	parser.href = reportUrl;
+	//something like that "https://it36.tribals.it/game.php?village=4745"
+	return parser.protocol+"//"+parser.host+parser.pathname+parser.search.split('&')[0];
+}
 
 function getCoordinates(destination){
 	return destination.substring(destination.indexOf('(')+1, destination.indexOf(')')).trim();
@@ -49,7 +61,47 @@ function getIconFileName(iconLink) {
 	return iconLink.substring(iconLink.lastIndexOf('/')+1);
 }
 
-function getResources(resources) {
+function getLinks(reportBaseUrl, playerId, villageId){
+	var playerUrl = reportBaseUrl + "&screen=info_player&id=" + playerId;
+	var villageUrl = reportBaseUrl + "&screen=info_village&id=" + villageId;
+
+	var link = {
+		villageLink: villageUrl,
+		playerLink: playerUrl
+	}
+	return link;
+}
+
+function getActions(reportBaseUrl, villageId){
+	var attackUrl = reportBaseUrl + "&screen=place&target=" + villageId;
+	var sendResourcesUrl = reportBaseUrl + "&screen=market&mode=send&target=" + villageId;
+	var targetMapUrl = reportBaseUrl + "&screen=map&beacon=1&id="+ villageId;	
+
+	var actions = {
+		attack: attackUrl,
+		sendResources: sendResourcesUrl,
+		targetMap: targetMapUrl
+	}
+
+	return actions;
+}
+
+function getResources(spiedResources, hauledResources) {
+	var spiedResourcesObj = parseResources(spiedResources);
+	var hauledResourcesObj = parseResources(hauledResources);
+
+	//Add transportable haul on hauled resources
+	hauledResourcesObj.transportableHaul = parseNumber($(hauledResources).find('td:last').text().trim().split('/')[1]);
+
+	result = {
+	    	 spied: spiedResourcesObj, 
+	    	 haul: hauledResourcesObj
+    	};
+
+	return result;
+}
+
+function parseResources(resources) {
 	if(!resources)
 		return {};
 	
@@ -60,6 +112,7 @@ function getResources(resources) {
 	var wood = parseNumber(woodStr);
 	var stone = parseNumber(stoneStr);
 	var iron = parseNumber(ironStr);
+
 	var total = wood + stone + iron;
 
 	var result = {
@@ -75,9 +128,10 @@ function getResources(resources) {
 function parseNumber(numberStr){
 	if(!numberStr ||  !Number.parseInt(numberStr))
 		return 0;
-	
+
 	return parseInt(numberStr.replace('.',''));
 }
+
 function getDate(localizedDate){
 	if(!localizedDate)
 		return null;
@@ -91,6 +145,22 @@ function getDate(localizedDate){
 	return aDate.toLocaleString();
 }
 
+function reportsEquality(report){
+
+	if(!this)
+		return false;
+
+	return this.id === report.id;
+
+}
+
+function storeReports(reportsToStore){
+	chrome.storage.local.set({reports: reportsToStore}, function() {
+	  // Notify that we saved.
+	  console.log('reports saved');
+});
+}
+
 /*{
 	"villageId": "4355",
 	"playerId": "464534",
@@ -100,8 +170,16 @@ function getDate(localizedDate){
 	"destination": "Villaggio di M--M (435|484) C44"
 	"coordinates": "456|467",
 	"reportIcon": "green",
-	"villageLink": "/game.php?village=4745&screen=info_village&id=4947",
-	"playerLink": "/game.php?village=4745&screen=info_player&id=713542",
+	"links": {
+		"villageLink": "https://it36.tribals.it/game.php?village=4745&screen=info_village&id=4947",
+		"playerLink": "https://it36.tribals.it/game.php?village=4745&screen=info_player&id=713542"
+	
+	},
+	actions: {
+		attack: "https://it36.tribals.it/game.php?village=4745&screen=place&target=3771&",
+		sendResources: "https://it36.tribals.it/game.php?village=4745&screen=market&mode=send&target=3771&",
+		targetMap: "https://it36.tribals.it/game.php?village=4745&screen=map&beacon=1&id=3771&"		
+	},
 	"resources" :{
 	"spied":{
 		"wood": 1000,
@@ -113,6 +191,7 @@ function getDate(localizedDate){
 		"wood": 100,
 		"stone": 200,
 		"iron": 220,
-		"total": 520
+		"total": 520,
+		"transportablHaul":2445
 	}}
 }*/
